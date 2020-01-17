6f8728eba4578921fd8d3b75544f35d9353c41d7
Aasim Sajjad <aas@ciklum.com>
2020-01-17 14:17:29 +0500

    deployment to netlify

--- src/App.js
+++ src/App.js
@@ -1,66 +1,41 @@
-import React, { Fragment, Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
+import NotFound from './components/pages/NotFound';
+import User from './components/users/User';
+import Home from './components/pages/Home';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
-import axios from 'axios';
+import GithubState from './context/github/githubState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-    alert: null
-  }
-
-  async componentDidMount() {
-    this.setState({ loading: true });
-    const result = await axios.get(`https://api.github.com/users?since=135&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({ users: result.data, loading: false });
-  }
-
-  searchUsers = async (text) => {
-    const result = await axios.get(`https://api.github.com/search/users?since=135&q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({ users: result.data.items, loading: false });
-  }
-
-  clearUsers = () => this.setState({ users: [], loading: false });
-
-  setAlert = (msg, type) => {
-    this.setState({ alert: {msg, type }});
-    setTimeout(() => this.setState({ alert: null }), 5000 );
-  }
-
-  render() {
-    const { loading, users } = this.state;
-    return (
-      <Router>
-      <div className="App">
-        <Navbar />
-        
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Switch>
-            <Route exact path='/' render={props => (
-              <Fragment>
-                <Search searchUsers={this.searchUsers } clearUsers={this.clearUsers} showClear={ users.length > 0 ? true: false } setAlert={this.setAlert} />
-                <Users loading={ loading } users={ users } />
-              </Fragment>
-            )} />
-            
-            <Route exact path='/about' component={About} />
-
-          </Switch>
+const App = () => {
+  
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+        <div className="App">
+          <Navbar />
           
+          <div className="container">
+            <Alert />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' component={User} />
+              <Route component={NotFound} />
+
+            </Switch>
+            
+          </div>
         </div>
-      </div>
-      </Router>
-    );
-  }
+        </Router>
+      </AlertState>
+    </GithubState>  
+  );
+ 
   
 }
 
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
+import GithubContext from '../../context/github/githubContext';
 
-const Users = ({ loading, users }) => {
+const Users = () => {
+
+    const githubContext = useContext(GithubContext);
+
+    const { loading, users } = githubContext;
 
     if(loading) {
         return <Spinner />
@@ -15,8 +20,6 @@ const Users = ({ loading, users }) => {
             </div>
         )
     }
-    
-
 }
 
 const userStyle = {
